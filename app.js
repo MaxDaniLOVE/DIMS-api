96963a2d7185e0c80ea371fe5cac1132461c9377
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-05-31 14:34:44 +0300

    update .gitignore

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send({"test": "test"}));
+app.get('/api/', (req, res) => res.send({"test": "test"}));
 
 app.listen(5000, () => {
-  console.log('http://localhost:5000/')
+  console.log('http://localhost:5000/api/')
 })
