e08612cc512941e59a704e856d795f89a9cdbf97
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-06-02 22:49:12 +0300

    fix: fix error handling in user tasks

--- controlers/user-task-controlers.js
+++ controlers/user-task-controlers.js
@@ -3,18 +3,24 @@ const Task = require('../models/task');
 const convertUserTaskData = require('../utils/convertUserTaskData');
 
 const getUserTasks = async (req, res, next) => {
-  const UserId = req.params.pid; 
-  const tasks = await UserTask.find({ UserId }).exec();
+  const UserId = req.params.pid;
+  let userTasks;
 
-  const convertedTasks = tasks.map(async ({ _doc: { TaskId, ...userTaskData } }) => {
-    const taskData = await Task.findById(TaskId).exec();
-    const { Name: TaskName, ...data } = taskData._doc;
-    const fullTaskData = { TaskName, UserId, TaskId, ...data, ...userTaskData };
-    const convertedTask = convertUserTaskData(fullTaskData);
-    return convertedTask;
-  });
+  try {
+    const tasks = await UserTask.find({ UserId }).exec();
 
-  const userTasks = await Promise.all(convertedTasks);
+    const convertedTasks = tasks.map(async ({ _doc: { TaskId, ...userTaskData } }) => {
+      const taskData = await Task.findById(TaskId).exec();
+      const { Name: TaskName, ...data } = taskData._doc;
+      const fullTaskData = { TaskName, UserId, TaskId, ...data, ...userTaskData };
+      const convertedTask = convertUserTaskData(fullTaskData);
+      return convertedTask;
+    });
+
+    userTasks = await Promise.all(convertedTasks);
+  } catch (error) {
+    return next(error);
+  }
 
   res.json(userTasks);
 };
@@ -26,31 +32,48 @@ const setTaskStatus = async (req, res, next) => {
     StatusId,
   }
    = req.body;
-  const { _id: UserTaskId } = await UserTask.findOne({ UserId, TaskId }).exec();
-  await UserTask.findByIdAndUpdate(UserTaskId, { StatusId });
+
+  try {
+    const { _id: UserTaskId } = await UserTask.findOne({ UserId, TaskId }).exec();
+    await UserTask.findByIdAndUpdate(UserTaskId, { StatusId });
+  } catch (error) {
+    return next(error);
+  }
+ 
   res.json({ message: 'succesfully updated' });
 };
 
 const addTaskToUser  = async (req, res, next) => {
   const TaskId = req.params.tid;
   const usersIds = req.body;
-  const { _doc: { Name: TaskName, _id, __v, ...data } } = await Task.findById(TaskId).exec();
-  usersIds.map(async (UserId) => {
-    const isExists = await UserTask.exists({ TaskId });
-    if (isExists) {
-      return null;
-    }
-    const createdUserTask = new UserTask({ UserId, TaskId, TaskName, StatusId: 1, ...data });
-    await createdUserTask.save();
-  })
+
+  try {
+    usersIds.map(async (UserId) => {
+      const isExists = await UserTask.exists({ TaskId, UserId });
+      if (isExists) {
+        return null;
+      }
+      const createdUserTask = new UserTask({ UserId, TaskId, StatusId: 1 });
+      await createdUserTask.save();
+    })
+  } catch (error) {
+    return next(error);
+  }
+  
   res.json({ message: "all users recieve new tasks with status 'Active'" });
 };
 
 const getAssignedUsers  = async (req, res, next) => {
   const TaskId = req.params.tid;
 
-  const result = await UserTask.find({ TaskId }, 'UserId').exec();
-  
+  let result;
+
+  try {
+    result = await UserTask.find({ TaskId }, 'UserId').exec();
+  } catch (error) {
+    return next(error);
+  }
+
   const assignedUser = result.map(({ UserId }) => UserId);
 
   res.json(assignedUser);
--- models/userTask.js
+++ models/userTask.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const userTaskSchema = new mongoose.Schema({
-  UserId: { type: String, required: true },
-  TaskId: { type: String, required: true },
-  StatusId: { type: Number, required: true }
+  UserId: { type: String, required: true, validate: /.{24,}/ },
+  TaskId: { type: String, required: true, validate: /.{24,}/ },
+  StatusId: { type: Number, required: true, validate:  /[1-3]{1}/ },
 });
 
 module.exports = mongoose.model('UserTask', userTaskSchema);
